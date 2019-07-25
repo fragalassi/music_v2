b077f21fc7a4071d31cc56ff21acae5cdb8c5ccf
Francesca Galassi <francesca.galassi@inria.fr>
2019-07-25 16:40:08 +0200

    First commit

--- call.sh
+++ call.sh
@@ -17,19 +17,19 @@ for patient in $DATADIR/*; do
 	T1gd=$DATADIR/"$patientID"/3DT1GADO.nii.gz
 	T2=$DATADIR/"$patientID"/T2.nii.gz
 
-	python3 animaMSExamPreparation.py -r $FLAIR -f $FLAIR -t $T1 -g $T1gd -T $T2
+	python3 animaMSExamPreparation.py -r $FLAIR -f $FLAIR -t $T1 -g $T1g -T $T2 -o $DATADIR/"$patientID"/ 
 
-#-o $DATADIR/"$patientID"/ 
-: '
-	#ADDITIONAL PREPROCESSING
+	#SEGMENTATION
 	#training/testing MICCAI16
 	FLAIR=$DATADIR/"$patientID"/3DFLAIR_preprocessed.nrrd
 	T1=$DATADIR/"$patientID"/3DT1_preprocessed.nrrd
+	T2=$DATADIR/"$patientID"/T2_preprocessed.nrrd
 	MASK=$DATADIR/"$patientID"/3DFLAIR_brainMask.nrrd
 	C=$DATADIR/"$patientID"/Consensus.nii.gz
 
-	#PYTHONHASHSEED=0 python3 $SCRIPTDIR/animaMusicLesionSegmentation.py -f $FLAIR -t $T1 -m $MASK -n 6 -o $DATADIR/"$patientID"/
-'
+	#python3 $SCRIPTDIR/animaMusicLesionSegmentation.py -f $FLAIR -t $T1 -T $T2 -m $MASK -o $DATADIR/"$patientID"
+
+
     fi
 done
 
